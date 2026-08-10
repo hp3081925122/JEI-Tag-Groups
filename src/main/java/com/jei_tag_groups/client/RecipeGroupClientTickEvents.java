311a9978c45377eb6b85207a968fa15a233da4b5
hp-1e <3081925122@qq.com>
2026-08-10 12:33:34 +0800

    chore: remove recipe key debug logging

--- src/main/java/com/jei_tag_groups/client/RecipeGroupClientTickEvents.java
+++ src/main/java/com/jei_tag_groups/client/RecipeGroupClientTickEvents.java
@@ -3,7 +3,6 @@ package com.jei_tag_groups.client;
 import com.jei_tag_groups.Jei_tag_groups;
 import com.jei_tag_groups.client.config.RecipeGroupManager;
 import com.jei_tag_groups.client.input.RecipeGroupKeyMappings;
-import com.mojang.logging.LogUtils;
 import mezz.jei.gui.recipes.RecipesGui;
 import net.minecraft.client.input.KeyEvent;
 import net.neoforged.api.distmarker.Dist;
@@ -14,12 +13,9 @@ import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.InputEvent;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
-import org.slf4j.Logger;
 
 @EventBusSubscriber(modid = Jei_tag_groups.MODID, value = Dist.CLIENT)
 public final class RecipeGroupClientTickEvents {
-    private static final Logger LOGGER = LogUtils.getLogger();
-
     private RecipeGroupClientTickEvents() {
     }
 
@@ -36,8 +32,6 @@ public final class RecipeGroupClientTickEvents {
             return;
         }
         Minecraft minecraft = Minecraft.getInstance();
-        LOGGER.debug("Recipe group key event: key={}, scanCode={}, action={}, screen={}", event.getKey(), event.getScanCode(), event.getAction(),
-            minecraft.screen == null ? "none" : minecraft.screen.getClass().getName());
         if (event.getAction() != GLFW.GLFW_PRESS || !(minecraft.screen instanceof RecipesGui)) {
             return;
         }
